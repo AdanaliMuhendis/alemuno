a4ad4831809c85b90c4fde4e28d0b169b3b64999
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-18 02:58:44 +0300

    Update promotions.py

--- promotions.py
+++ promotions.py
@@ -7,7 +7,7 @@ import random
 # Promotion messages and their weights
 PROMOTIONS = {
     """
-For a more modern UNO experience, <a href="https://t.me/uno9bot/uno">try out</a> the new <a href="https://t.me/uno9bot?start=ref-unobot">@uno9bot</a>.
+For a more modern UNO experience, <a href="https://t.me/AlemUnoBot/uno">try out</a> the new <a href="https://t.me/AlemUnoBot?start=ref-unobot">@AlemUnoBot</a>.
 """: 2.0,
     """
 Also check out @UnoDemoBot, a newer version of this bot with exclusive modes and features!
